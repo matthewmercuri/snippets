f1351b0d3b8b40d27c444262cf2d5f70069945ce
Matthew Mercuri <mercurimatthew@gmail.com>
2020-11-14 22:01:07 -0500

    fant table function

--- NFLDATA/main.py
+++ NFLDATA/main.py
@@ -1,7 +1,18 @@
-import pandas
+from bs4 import BeautifulSoup
+import pandas as pd
 import requests
 
+BASE_URL = 'https://www.pro-football-reference.com/'
+YEAR = 2020
 
-class RawData:
-    def __init__(self):
-        pass
+
+def get_fantasy_table(year=YEAR):
+    URL = BASE_URL + f'years/{year}/fantasy.htm'
+    r = requests.get(URL)
+    soup = BeautifulSoup(r.text, 'lxml')
+
+    dfs = pd.read_html(soup.prettify())
+    print(dfs)
+
+
+get_fantasy_table()
